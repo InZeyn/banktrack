bf0e23acef8bdc5979f284ca8b0245ea9a40abb6
InZeyn <silvstedtz@gmail.com>
2022-07-02 22:18:41 -0400

    root and app folder cleanup

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Container } from '@mui/material';
 import "@fontsource/roboto";
 import { ThemeProvider } from '@mui/material';
 import theme from './theme';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TransactionDetails from './pages/TransactionDetails';
+import CreateTransaction from './pages/CreateTransaction';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <React.StrictMode>
-      <Container >
-        <App />
-      </Container>
-    </React.StrictMode>
-  </ThemeProvider>
+  <Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <React.StrictMode>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="transaction/:id" element={<TransactionDetails />} />
+          <Route path="transaction/create" element={<CreateTransaction />} />
+        </Routes>
+      </React.StrictMode>
+    </ThemeProvider>
+  </Router>
 );
